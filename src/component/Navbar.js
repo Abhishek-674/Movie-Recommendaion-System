5ba03fecb1fac02ba8634831065d3f5cb7afdd39
Abhishek <abhishek.verma674@gmail.com>
2024-10-30 15:00:15 +0530

    final project after finding bug

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -36,14 +36,8 @@ const Navbar = () => {
   return (
     <div className="absolute top-0 left-0 flex w-full h-24 items-center justify-between text-white px-5 md:px-10 z-50 bg-opacity-80 bg-black backdrop-blur">
       <div className="flex items-center">
-        <img className="w-10 md:w-20" src={logo2} alt="Logo" />
-        {/* <ul className="hidden md:flex space-x-6 ml-8">
-           <Link to="/b"> <li className="cursor-pointer hover:text-red-400 transition-colors">Home</li></Link>
-         
-          <Link to="/search"><li className="cursor-pointer hover:text-red-400 transition-colors">Search Movie</li></Link>
-          <li className="cursor-pointer hover:text-red-400 transition-colors">TV Shows</li>
-          <li className="cursor-pointer hover:text-red-400 transition-colors">News</li>
-        </ul> */}
+        <img className="w-14 md:w-20" src={logo2} alt="Logo" />
+       
       </div>
       
       <div className="flex items-center space-x-6">
@@ -58,22 +52,15 @@ const Navbar = () => {
           
           <span className="block md:hidden">Search</span>
         </button>
-
-        {/* <button
-          onClick={logout_handler}
-          className="bg-gradient-to-r from-red-400 to-red-600 hover:from-red-500 hover:to-red-700 text-white font-semibold rounded-lg shadow-lg transition duration-300 ease-in-out px-5 py-2.5 flex items-center"
-        >
-          Logout
-        </button> */}
-        
-        <div className="flex justify-center flex-col items-center">
+        <div  onClick={logout_handler} className="flex justify-center flex-col items-center ">
           <img
-             onClick={logout_handler}
+            
             className="w-8 h-8 rounded-full"
             src="https://img.freepik.com/premium-photo/stylish-man-flat-vector-profile-picture-ai-generated_606187-310.jpg?semt=ais_hybrid"
             alt="Profile"
           />
-          <span>{mail} (logout)</span>
+          <h6 className="text-sm">{mail}</h6>
+          <h6 className="text-sm">Logout</h6>
           {/* <h1 className=""></h1> */}
         </div>
       </div>
